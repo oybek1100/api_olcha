7fe0c638beb2374b552b2d440a265d779cec7543
diyor1100 <mamatqulovdiyor03@gmail.com>
2025-07-26 00:22:41 +0500

    allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["api-olchaaa.onrender.com", "127.0.0.1", "localhost"]
+
 
 
 # Application definition
